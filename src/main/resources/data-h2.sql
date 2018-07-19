b36d4d729a5b203d8d17340dd9d52ccf21ed963f
Anthony Farina <afarina@guidewire.com>
2018-07-19 12:54:15 -0400

    logging clean up

--- src/main/resources/data-h2.sql
+++ src/main/resources/data-h2.sql
@@ -7,3 +7,5 @@ SET @currentDate = GETDATE();
 INSERT INTO office(name, created_at, last_modified_at) VALUES('Exton', @currentDate, @currentDate);
 INSERT INTO office(name, created_at, last_modified_at) VALUES('Dublin', @currentDate, @currentDate);
 INSERT INTO office(name, created_at, last_modified_at) VALUES('Foster City', @currentDate, @currentDate);
+
+insert INTO user(name, username, password, created_at, last_modified_at) VALUES ('test', 'test', '$2a$04$JfG91YTzI/CpHEY8GAo4/.owASEYkmXb44fNLmD0PNkMYPtJe4nr.', @currentDate, @currentDate);
